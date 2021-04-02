7516b2605ebf856c89185176b4994a87ae897d6c
Tim Wu <t1mtimmy@live.com>
2021-04-02 14:56:25 -0400

    Wrote functions to calculate uni/bi/tri-gram counts.

--- hw3/a3_wu_112550028.py
+++ hw3/a3_wu_112550028.py
@@ -30,12 +30,9 @@ def getTargetWord(context):
     return headIndex
 
 def readData(file):
-    # senses dict maps senses to unique integers
-    # counts maps words to their frequencies.
-    # process/machine/language store their respective lines of data.
 
-    data = {"senses": {}, "counts": {}, "lemmas":[]}
-    sensecounter = 0
+    data = {"counts": {}, "contexts": []}
+
     with open(file, mode="r", encoding='utf-8') as f:
         for line in f.readlines():
 
@@ -57,8 +54,18 @@ def readData(file):
                     
                 data["counts"][token] = data["counts"][token] + 1
             
+            # add <s> and </s> tokens
+            if "<s>" not in data["counts"].keys():
+                data["counts"]["<s>"] = 0
+
+            if "</s>" not in data["counts"].keys():
+                data["counts"]["</s>"] = 0
+            
+            data["counts"]["<s>"] = data["counts"]["<s>"] + 1
+            data["counts"]["</s>"] = data["counts"]["</s>"] + 1
+
             # Remove lemma and POS
-            finalcontext = '<s>'
+            finalcontext = '<s> '
             i = 0
             for word in curcontext.split(" "):
                 token = word.split("/")[0].lower()
@@ -68,44 +75,201 @@ def readData(file):
 
             finalcontext = finalcontext.strip()
             
-            # check lemma to properly classify the tuple we're going to create.
-            lemma = curline[0].split(".")[0]
+            finalcontext += " </s>"
 
-            # Assign an integer to this sense if we haven't seen it yet in the current lemma.
-            sense = curline[1]
-            if lemma not in data["senses"].keys():
-                data["senses"][lemma] = {}
-
-            if sense not in data["senses"][lemma].keys():
-                data["senses"][lemma][sense] = sensecounter % 6
-                sensecounter += 1
-
-            # if a list for this lemma does not exist, we need to make it.
-            # list of tuples that goes (headIndex, context, sense (string rep), unique context ID)
-            if lemma not in data.keys():
-                data[lemma] = [] # create list for lemma
-                data["lemmas"].append(lemma) # also add it to the list of lemmas.
-
-            finalcontext += "</s>"
-            data[lemma].append((headIndex, finalcontext, sense, lineID))
+            data["contexts"].append(finalcontext.split(" "))
 
         return data
 
+# counts has the count of every single token in our corpus.
+# vocab has only the 5000 most frequent tokens in our corpus.
+def extractUnigramCounts(counts: dict, vocab: list):
+    
+    unigrams = {}
+    unigrams["<OOV>"] = 0
+
+    for key in counts.keys():
+        if key not in vocab:
+            unigrams["<OOV>"] += counts[key]
+        else:
+            unigrams[key] = counts[key]
+
+    return unigrams
+
+# Create the counts in a sparse dictionary of dictionaries.
+def extractBigramCounts(contexts: list, vocab: list):
+    counts = {}
+    for context in contexts:
+        for i in range(0, len(context) - 1):
+            word1 = context[i]
+            word2 = context[i + 1]
+            
+            # both oov
+            if (word1 not in vocab) and (word2 not in vocab):
+                counts.setdefault("<OOV>", {}) # add OOV for first word
+                counts["<OOV>"].setdefault("<OOV>", 0) # add OOV for second word
+
+                counts["<OOV>"]["<OOV>"] += 1
+            
+            # first word oov
+            elif word1 not in vocab:
+                counts.setdefault("<OOV>", {}) # add OOV for first word
+                counts["<OOV>"].setdefault(word2, 0)
+
+                counts["<OOV>"][word2] += 1
+
+            #second word oov
+            elif word2 not in vocab:
+                counts.setdefault(word1, {})
+                counts[word1].setdefault("<OOV>", 0) # add OOV for second word
+
+                counts[word1]["<OOV>"] += 1
+
+            # none oov
+            else:
+                counts.setdefault(word1, {})
+                counts[word1].setdefault(word2, 0)
+    
+                counts[word1][word2] += 1
+
+    return counts
+
+def extractTrigramCounts(contexts: list, vocab: list):
+    counts = {}
+    for context in contexts:
+        for i in range(0, len(context) - 2):
+            word1 = context[i] 
+            word2 = context[i + 1]
+            word3 = context[i + 2]
+            
+            # [(oov, oov)][oov] - all out of vocab = 000
+            if (word1 not in vocab) and (word2 not in vocab) and (word3 not in vocab):
+                counts.setdefault(("<OOV>", "<OOV>"), {})
+                counts[("<OOV>", "<OOV>")].setdefault("<OOV>", 0)
+
+                counts[("<OOV>", "<OOV>")]["<OOV>"] += 1
+
+            # [(oov, oov)][word3] = 001
+            elif (word1 not in vocab) and (word2 not in vocab) and (word3 in vocab):
+                counts.setdefault(("<OOV>", "<OOV>"), {})
+                counts[("<OOV>", "<OOV>")].setdefault(word3, 0)
+
+                counts[("<OOV>", "<OOV>")][word3] += 1
+
+            # [(oov, word2)][oov] = 010
+            elif (word1 not in vocab) and (word2 in vocab) and (word3 not in vocab):
+                counts.setdefault(("<OOV>", word2), {})
+                counts[("<OOV>", word2)].setdefault("<OOV>", 0)
+
+                counts[("<OOV>", word2)]["<OOV>"] += 1
+
+            # [(oov, word2)][word3] = 011
+            elif (word1 not in vocab) and (word2 in vocab) and (word3 in vocab):
+                counts.setdefault(("<OOV>", word2), {})
+                counts[("<OOV>", word2)].setdefault(word3, 0)
+
+                counts[("<OOV>", word2)][word3] += 1
+
+            # [(word1, oov)][oov] = 100
+            elif (word1 in vocab) and (word2 not in vocab) and (word3 not in vocab):
+                counts.setdefault((word1, "<OOV>"), {})
+                counts[(word1, "<OOV>")].setdefault("<OOV>", 0)
+
+                counts[(word1, "<OOV>")]["<OOV>"] += 1
+
+            # [(word1, oov)][word3] = 101
+            elif (word1 in vocab) and (word2 not in vocab) and (word3 in vocab):
+                counts.setdefault((word1, "<OOV>"), {})
+                counts[(word1, "<OOV>")].setdefault(word3, 0)
+
+                counts[(word1, "<OOV>")][word3] += 1
+
+            # [(word1, word2)][oov] = 110
+            elif (word1 in vocab) and (word2 in vocab) and (word3 not in vocab):
+                counts.setdefault((word1, word2), {})
+                counts[(word1, word2)].setdefault("<OOV>", 0)
+
+                counts[(word1, word2)]["<OOV>"] += 1
+
+            # [(word1, word2)][word3] - all in vocab = 111
+            else:
+                counts.setdefault((word1, word2), {})
+                counts[(word1, word2)].setdefault(word3, 0)
+
+                counts[(word1, word2)][word3] += 1
+
+    return counts
 def main():
 
-    traindata = readData(sys.argv[1]) # use to train
+    data = readData(sys.argv[1]) # use to train
 
-    debug = True
+    debug = False
     if debug:
-        for part in traindata:
+        for part in data:
             print("{0}: ".format(part))
-            print(traindata[part])
+            print(data[part])
             print()
 
     # Create list of 5000 most frequent words.
-    countsSorted = sorted(traindata["counts"].items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+    countsSorted = sorted(data["counts"].items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
     countsSorted = countsSorted[:5000] # we only want the 5000 most frequent ones.
-    countsSorted = [tup[0] for tup in countsSorted]
+    
+    vocab = [tup[0] for tup in countsSorted]
+
+    unigram_counts = extractUnigramCounts(data["counts"], vocab)
+
+    bigram_counts = extractBigramCounts(data["contexts"], vocab)
+
+    trigram_counts = extractTrigramCounts(data["contexts"], vocab)
+    #for key in trigram_counts.keys():
+        #print(key)
+        #print("\t" + str(trigram_counts[key]))
+    
+    print("UNIGRAMS:")
+    try:
+        print("language: " + str(unigram_counts["language"]))
+    except KeyError:
+        print("language: 0")
+    try:
+        print("the:" + str(unigram_counts["the"]))
+    except KeyError:
+        print("the: 0")
+    try:
+        print("formal:" + str(unigram_counts["formal"]))
+    except KeyError:
+        print("formal: 0")
+    print("\n")
+
+
+    print("BIGRAMS:")
+    try:
+        print("(the, language): " + str(bigram_counts["the"]["language"]))
+    except KeyError:
+        print("(the, language): 0")
+    try:
+        print("(<OOV>, language): " + str(bigram_counts["<OOV>"]["language"]))
+    except KeyError:
+        print("(<OOV>, language): 0")
+    try:
+        print("(to, process): " + str(bigram_counts["to"]["process"]))
+    except KeyError:
+        print("(to, process): 0")
+    print("\n")
+
+
+    print("TRIGRAMS:")
+    try:
+        print("(specific, formal, languages: " + str(trigram_counts[("specific", "formal")]["languages"]))
+    except KeyError:
+        print("(specific, formal, languages: 0")
+    try:
+        print("(to, process, <OOV>: " + str(trigram_counts[("to", "process")]["<OOV>"]))
+    except KeyError:
+        print("(to, process, <OOV>: 0")
+    try:
+        print("(specific, formal, event: " + str(trigram_counts[("specific", "formal")]["event"]))
+    except KeyError:
+        print("(specific, formal, event: 0")
 
 if __name__ == "__main__":
     main()
